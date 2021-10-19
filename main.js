01b20fcd0a74f7c182412b89ae1802daac3aa22d
Marcell <maci20506@gmail.com>
2021-10-19 14:35:15 +0200

    Üres értéket nem lehet hozzáadni a listához

--- main.js
+++ main.js
@@ -8,27 +8,33 @@ class Film {
 }
 
 function Hozzaad() {
-     let nev = document.getElementById("inputNev").value;
-     let rendezo = document.getElementById("inputRendezo").value;
-     let kiadasiEv = document.getElementById("inputkiadasiEv").value;
-     let film1 = new Film(nev, rendezo, kiadasiEv);
-     filmTomb.push(film1);
+     if (document.getElementById("inputNev").value == "" ||
+     document.getElementById("inputRendezo").value == "" ||
+     document.getElementById("inputkiadasiEv").value == "") {
+          alert("Kérem");
+     } else {
+          let nev = document.getElementById("inputNev").value;
+          let rendezo = document.getElementById("inputRendezo").value;
+          let kiadasiEv = document.getElementById("inputkiadasiEv").value;
+          let film1 = new Film(nev, rendezo, kiadasiEv);
+          filmTomb.push(film1);
 
-     document.getElementById("tablazatNev").innerHTML = "";
-     document.getElementById("tablazetRendezo").innerHTML = "";
-     document.getElementById("tablazatEv").innerHTML = "";
+          document.getElementById("tablazatNev").innerHTML = "";
+          document.getElementById("tablazetRendezo").innerHTML = "";
+          document.getElementById("tablazatEv").innerHTML = "";
 
-     for (let i = 0; i < filmTomb.length; i++) {
-          document.getElementById("tablazatNev").innerHTML += filmTomb[i].nev + "<br>";
-          document.getElementById("tablazetRendezo").innerHTML += filmTomb[i].rendezo + "<br>";
-          document.getElementById("tablazatEv").innerHTML += filmTomb[i].kiadasiEv + "<br>";
+          for (let i = 0; i < filmTomb.length; i++) {
+               document.getElementById("tablazatNev").innerHTML += filmTomb[i].nev + "<br>";
+               document.getElementById("tablazetRendezo").innerHTML += filmTomb[i].rendezo + "<br>";
+               document.getElementById("tablazatEv").innerHTML += filmTomb[i].kiadasiEv + "<br>";
+          }
+
+          document.getElementById("inputNev").value = "";
+          document.getElementById("inputRendezo").value = "";
+          document.getElementById("inputkiadasiEv").value = "";
+
+          szamol = 0;
      }
-
-     document.getElementById("inputNev").value = "";
-     document.getElementById("inputRendezo").value = "";
-     document.getElementById("inputkiadasiEv").value = "";
-
-     szamol = 0;
 }
 
 document.getElementById("gombKuld").addEventListener("click", Hozzaad);
